9df92fc3ee6607b58f986c49acf7447f6566a572
diadntjr <dntjr0501@gmail.com>
2022-07-24 23:56:21 +0900

    Update bottom_bar.dart
    Remove SizedBar
    -> 컨테이너로 높이 지정하는걸로 변경했어요~

--- lib/widget/bottom_bar.dart
+++ lib/widget/bottom_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:bssmflat/common/common.dart';
 
 class Bottom extends StatelessWidget {
   const Bottom({Key? key}) : super(key: key);
@@ -7,44 +8,44 @@ class Bottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 60.h,
       decoration: BoxDecoration(
+        color: Colors.transparent,
         border: Border.all(
-          color: Colors.black, // red as border color
+          color: CommonColor.gray02, // red as border color
         ),
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(31.r), topRight: Radius.circular(31.r))
       ),
-      child: SizedBox(
-        height: 60.h,
-        child: TabBar(
-          labelColor: const Color(0xff00A9A5),
-          unselectedLabelColor: Colors.black,
-          indicatorColor: Colors.transparent,
-          tabs: [
-            Tab(
-              icon: Icon(
-                Icons.home_outlined,
-                size: 45.h,
-              ),
+      child: TabBar(
+        labelColor: const Color(0xff00A9A5),
+        unselectedLabelColor: Colors.black,
+        indicatorColor: Colors.transparent,
+        tabs: [
+          Tab(
+            icon: Icon(
+              Icons.home_outlined,
+              size: 45.h,
             ),
-            Tab(
-              icon: Icon(
-                Icons.feed_outlined,
-                size: 45.h,
-              ),
+          ),
+          Tab(
+            icon: Icon(
+              Icons.feed_outlined,
+              size: 45.h,
             ),
-            Tab(
-              icon: Icon(
-                Icons.edit_outlined,
-                size: 45.h,
-              ),
+          ),
+          Tab(
+            icon: Icon(
+              Icons.edit_outlined,
+              size: 45.h,
             ),
-            Tab(
-              icon: Icon(
-                Icons.account_circle_outlined,
-                size: 45.h,
-              ),
+          ),
+          Tab(
+            icon: Icon(
+              Icons.account_circle_outlined,
+              size: 45.h,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
