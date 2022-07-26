c2acc196b9843e61eb413e80e9d88e6194184daf
조용제 <whehdrms6810@naver.com>
2022-07-27 02:38:43 +0900

    [CREATE] studypaper_screen.dart

---
+++ lib/screen/detailPaper_screen.dart
@@ -0,0 +1,19 @@
+import 'package:bssmflat/model/study_paper.dart';
+import 'package:flutter/material.dart';
+
+class DetailPaper extends StatefulWidget {
+  final Paper papers;
+  DetailPaper({required this.papers});
+
+  @override
+  State<DetailPaper> createState() => _DetailPaperState();
+}
+
+class _DetailPaperState extends State<DetailPaper> {
+  @override
+  Widget build(BuildContext context) {
+    return (
+      Column()
+    );
+  }
+}
